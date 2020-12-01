d69ff47a580c21c19653a9247bddd4171fd2f573
sawyerkking <74156027+sawyerkking@users.noreply.github.com>
2020-11-30 20:35:36 -0500

    Question1Part2

---
+++ Question1Part2/Client.py
@@ -0,0 +1,27 @@
+import socket
+#create socket
+client=socket.socket()
+
+#get ip
+host=socket.gethostname()
+hostip=socket.gethostbyname(host)
+
+#connect to server
+client.connect((hostip,4321))
+
+while 1:
+#sending a message to server
+    print("Enter a message to send. Or request to check the Host port type 'portcheck'")
+    data=input()
+    client.sendall(data.encode('ascii', 'strict'))
+
+#receiving a message from the server.
+    received=client.recv(1024).decode('ascii', 'strict')
+    print(received)
+    if input()=="quit":
+        break
+
+
+
+
+
